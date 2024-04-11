3678b69e1bb1b37f16fb9a924793a8d92d60e96d
LucasForti <lukas_discovery@hotmail.com>
2024-04-10 22:11:02 -0300

    feat: Criado fluxo de delete no pet

--- src/pet/pet.tokens.ts
+++ src/pet/pet.tokens.ts
@@ -1,7 +1,10 @@
 export default class PetTokens {
+    
     static createPetUseCase = 'createPetUseCase';
     static getPetByIdUseCase = 'getPetByIdUseCase';
     static petRepository = 'petRepository';
     static updatePetByIdUseCase = 'updatePetByIdUseCase'
+    static deletePetByIdUseCase = 'deletePetByIdUseCase'
+    
   }
   
---
+++ src/pet/usecases/dtos/delete.pet.by.id.usecase.input.ts
@@ -0,0 +1,8 @@
+export default class DeletePetByIdUseCaseInput {
+    id: string
+
+    constructor (data: Partial<DeletePetByIdUseCaseInput>){
+        Object.assign(this, data)
+  }
+    
+}
